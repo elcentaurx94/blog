e3b03abd419fb9d03f6417d89cfde73a03d4d9be
Daniel Marquez <daniel.marquez@ursa.net>
2020-03-27 08:48:23 -0600

    first commit

---
+++ lib/discuss_web/models/topic.ex
@@ -0,0 +1,18 @@
+defmodule Discuss.Topic do
+  use DiscussWeb, :model
+ # use Ecto.Schema
+  ##import Ecto.Changeset
+
+  schema "topics" do
+    field :title, :string
+  end
+
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+  end
+
+end
+
+
