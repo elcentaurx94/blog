6c815619efafbd43915578c0f8e0490c71283f98
Daniel Marquez <daniel.marquez@ursa.net>
2020-03-30 10:47:51 -0600

    Error_error_tag

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -9,9 +9,10 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{},topic)
 
-    case Repo.insert(changeset) do
+    case Discuss.Repo.insert(changeset) do
       {:ok, post} ->IO.inspect(post)
-      {:error,changeset}-> IO.inspect(changeset)
+      {:error,changeset}->  #IO.inspect(changeset)
+        render conn, "new.html", changeset: changeset
     end
   end
 end
